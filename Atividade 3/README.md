9d1771c1bf66b81ebd7b10c437bd21d4c37d055f
Eduardo Ribeiro <1600820@estudante.uab.pt>
2020-04-27 16:48:12 +0100

    Implemented mpi-repvc

---
+++ Atividade 3/README.md
@@ -0,0 +1,7 @@
+# cad-atividade3
+Projects for Activity #3 of High Performance Computation (Computação de Alto Desempenho) 
+
+##mpi-repvc
+Reimplement the array sharing exercise from Atividade 1/mpi-repvb but using MPI_Bcast
+
+
---
+++ Atividade 3/mpi-repvc/repvc.c
@@ -0,0 +1,78 @@
+/**
+ * This program shares an array between all the processes in the pool
+ * using MPI_Bcast
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+// Tag to use on our messages
+#define MESSAGE_TAG 1
+
+int main(int argc, char *argv[])
+{
+    int npes, myrank;
+    int currentPower = 1, n = 0;
+    int *data;
+    MPI_Status status;
+
+    int error = MPI_Init(&argc, &argv);
+    if (error != MPI_SUCCESS)
+    {
+        printf("Error initializing MPI environment!\n");
+        exit(1);
+    }
+
+    MPI_Comm_size(MPI_COMM_WORLD, &npes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+    if (argc < 2)
+    {
+        // Only process #0 reports the error to avoid spamming the command line
+        if (myrank == 0)
+        {
+            printf("Usage: %s ARRAY_LENGTH\n", argv[0]);
+        }
+
+        MPI_Finalize();
+        return 0;
+    }
+
+    n = atoi(argv[1]);
+
+    if (n < 3)
+    {
+        // Only process #0 reports the error to avoid spamming the command line
+        if (myrank == 0)
+        {
+            printf("Usage: %s ARRAY_LENGTH\nARRAY_LENGTH >= 3\n", argv[0]);
+        }
+
+        MPI_Finalize();
+        return 0;
+    }
+
+    data = malloc(sizeof(int) * n);
+
+    if (myrank == 0)
+    {
+        // Process #0 initializes the data array
+        for (int i = 0; i < n; i++)
+        {
+            data[i] = i;
+        }
+
+        
+    }
+    
+    MPI_Bcast(data, n, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    // Print (part of) the array
+    printf("Processo %d: v[%d] = %d, %d, ..., %d\n", myrank, n, data[0], data[1], data[n - 1]);
+
+    free(data);
+
+    MPI_Finalize();
+    return 0;
+}
